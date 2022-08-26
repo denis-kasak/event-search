27ae56347c361a67b25904f89d6caec270ad8ee7
denis-kasak <d-kasakow@mail.de>
2022-08-26 22:18:15 +0200

    init

---
+++ src/scraper.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ReadWebPage {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://berlinischegalerie.de/ausstellungen/aktuell/"))
+                .GET() // GET is default
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
