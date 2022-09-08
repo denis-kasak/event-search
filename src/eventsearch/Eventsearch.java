cca3432440633ee8951f5f1be178f14a3da53dd6
denis-kasak <91327614+denis-kasak@users.noreply.github.com>
2022-09-08 09:11:33 +0200

    getHtml() und cutTag() Methode hinzugefügt

--- src/eventsearch/Eventsearch.java
+++ src/eventsearch/Eventsearch.java
@@ -1,9 +1,11 @@
 package eventsearch;
 
+import java.io.IOException;
+
 public class Eventsearch {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Scraper.getHtml();
 
 	}
 
--- src/eventsearch/Scraper.java
+++ src/eventsearch/Scraper.java
@@ -1,5 +1,39 @@
 package eventsearch;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.IOException;
+
 public class Scraper {
+	
+	public static void getHtml() throws IOException, InterruptedException {
+		
+		HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://berlinischegalerie.de/ausstellungen/"))
+                .GET() // GET is default
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+        
+        String s = response.body();
+        
+        int i = s.indexOf("<body>");
+        int k = s.lastIndexOf("</body>");
+        
+        s = cutTag(response.body(), "<body>", "</body>");
+		
+		
+		return;
+	}
+	
+	private static String cutTag(String doc, String tag1, String tag2) {
+		
+		String newDoc = doc.substring(doc.indexOf(tag1)+tag1.length(), doc.indexOf(tag2));
+		return newDoc;
+	}
 
 }
