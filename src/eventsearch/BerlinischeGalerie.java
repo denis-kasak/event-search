b1ad00b3373e7ba01cc03bf55c23df4f432cd8b4
denis-kasak <91327614+denis-kasak@users.noreply.github.com>
2022-09-11 00:26:08 +0200

    Extraktion aller Ausstellungen von BG funktioniert

--- src/eventsearch/BerlinischeGalerie.java
+++ src/eventsearch/BerlinischeGalerie.java
@@ -1,61 +1,46 @@
 package eventsearch;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BerlinischeGalerie extends EventOwner {
 
-	public BerlinischeGalerie() throws IOException, InterruptedException {
+	public BerlinischeGalerie() throws Exception {
 		String doc = Scraper.getHtmlDoc("https://berlinischegalerie.de/ausstellungen/");
-		segment(doc);
+		ArrayList<String> segments = new ArrayList<String>();
+		segments = segment(doc);
+		getEvents(segments);
 
 	}
 
-	public ArrayList<ArrayList<String>> getEvents() throws Exception { // returns all events from
+	public ArrayList<ArrayList<String>> getEvents(ArrayList<String> segments) throws Exception { // returns all events
+																									// from
 		// Berlinische Galerie
 		ArrayList<ArrayList<String>> events = new ArrayList<ArrayList<String>>();
-		String doc = Scraper.getHtmlDoc("https://berlinischegalerie.de/ausstellungen/");
 
-		ArrayList<String> event = new ArrayList<String>();
-		ArrayList<String> pattern = new ArrayList<String>();
+		for (String i : segments) {
+			ArrayList<String> event = new ArrayList<String>();
+			Pattern pattern = Pattern.compile("<[^>]*>");
+			Matcher match = pattern.matcher(i);
+			match.find();
+			int begin = match.end();
+			while (match.find()) {
+				int end = match.start();
 
-		Pattern patt1 = Pattern.compile("<div class=\"o-grid-floaty__text\">");
-		Matcher match1 = patt1.matcher(doc);
-		int i = 0;
-		while (match1.find()) {
-			System.out.println(i);
-			// Author
-			pattern.add("<div class=\"o-grid-floaty__text\">");
-			pattern.add("<span>");
-			doc = Scraper.cutToInfo(doc, pattern);
-			event.add(Scraper.cutEnd(doc, "</span>"));
-
-			// Titel
-			pattern.clear();
-			pattern.add("<p>");
-			doc = Scraper.cutToInfo(doc, pattern);
-			event.add(Scraper.cutEnd(doc, "<br>"));
-
-			// Untertitel
-			pattern.clear();
-			pattern.add("<br>");
-			doc = Scraper.cutToInfo(doc, pattern);
-			event.add(Scraper.cutEnd(doc, "</p>"));
-
-			// Zeitangabe
-			pattern.clear();
-			pattern.add("<p>");
-			doc = Scraper.cutToInfo(doc, pattern);
-			String time = Scraper.cutEnd(doc, "<time");
-			pattern.clear();
-			pattern.add("<time datetime=");
-			pattern.add(">");
-			doc = Scraper.cutToInfo(doc, pattern);
-			event.add(time + " " + Scraper.cutEnd(doc, "</time>"));
-
+				if (match.end() == i.length()) {
+					break;
+				}
+				if (i.substring(begin, end).trim().equals("")) {
+					begin = match.end();
+					continue;
+				} else {
+					event.add(i.substring(begin, end).trim());
+					begin = match.end();
+				}
+			}
 			events.add(event);
+
 		}
 
 		return events;
@@ -69,9 +54,9 @@ public class BerlinischeGalerie extends EventOwner {
 		while (match.find()) {
 			int divCounter = 0;
 			for (int i = match.start(); i < doc.length(); i++) {
-				if (doc.substring(i, i+4).equals("<div")) {
+				if (doc.substring(i, i + 4).equals("<div")) {
 					divCounter++;
-				} else if (doc.substring(i, i+5).equals("</div")) {
+				} else if (doc.substring(i, i + 5).equals("</div")) {
 					divCounter--;
 				}
 				if (divCounter == 0) {
--- src/eventsearch/EventOwner.java
+++ src/eventsearch/EventOwner.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public abstract class EventOwner {
 	
-	public abstract ArrayList<ArrayList<String>> getEvents() throws Exception;
+	public abstract ArrayList<ArrayList<String>> getEvents(ArrayList<String> segments) throws Exception;
 
 	protected abstract ArrayList<String> segment(String doc);
 
