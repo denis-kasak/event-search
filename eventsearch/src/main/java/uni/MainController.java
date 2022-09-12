e40e897cd55352730d1e9502fd3ad1c8adbbd837
denis-kasak <91327614+denis-kasak@users.noreply.github.com>
2022-09-12 19:57:54 +0200

    MVC Setup

---
+++ eventsearch/src/main/java/uni/MainController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uni;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author d-kas
+ */
+public class MainController {
+    @FXML
+    private void test(){
+        System.out.println("test erfolgreich");
+    }
+}
